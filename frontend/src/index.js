5d88d4b1964fbf7de8d6eb061bfcea144abaa96a
LucasMasinaUNSW <z5204754@unsw.edu.au>
2021-01-02 18:35:23 +1100

    more setup stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import Welcome from './components/welcome';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import Welcome from "./components/welcome";
 
-ReactDOM.render(
-  <Welcome/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Welcome />, document.getElementById("root"));
